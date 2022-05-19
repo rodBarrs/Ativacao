7b22bbd817e36a2cebf4fbc68c0ae2b2584564f0
Rafael Henrique <fel.rique70@gmail.com>
2022-05-19 14:23:02 -0300

    front parar triagem funcionando 121

--- src/main/java/com/samirAtivacao/view/Selecao.java
+++ src/main/java/com/samirAtivacao/view/Selecao.java
@@ -109,6 +109,7 @@ public class Selecao extends javax.swing.JFrame {
 		loginbremiz.setLocationRelativeTo(null);
 		loginbremiz.setVisible(true);
             loginbremiz.setResizable(false);
+            //
 		}
 		 else{
 		loginbremiz.setLocationRelativeTo(null);
